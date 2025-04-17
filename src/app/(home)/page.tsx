c468bf045c75c92a6f0510ea240882eb219efc50
Júnior Nascimento <krymancer@protonmail.com>
2025-04-16 22:18:27 -0300

    Update zod dependency and enhance UI components
    - Updated zod dependency from version 3.24.2 to 3.24.3 in package.json and bun.lock.
    - Refactored various UI components, including Button, Input, Textarea, and Checkbox, to improve styling and functionality.
    - Enhanced global styles in globals.css with additional CSS variables and improved layout structures.
    - Removed deprecated ImageCard and Marquee components to streamline the codebase.
    - Added new features to existing components, such as improved accessibility and responsiveness.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,35 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+
+import { Button } from "@/components/ui/button";;
 import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("I'm a input");
+  const [textArea, setTextArea] = useState("I'm a textarea");
   return (
     <div className="p-4">
       <div className="flex flex-col gap-y-4">
         <div>
-          <Button>
+          <Button variant="elevated">
             I am a button
           </Button>
         </div>
         <div>
-          <Input placeholder="I am an input" />
+          <Input placeholder={text} onChange={(e) => setText(e.target.value)} />
         </div>
         <div>
           <Progress value={50} />
         </div>
         <div>
-          <Textarea value={"I am a textarea"} />
+          <Textarea value={textArea} onChange={(e) => setTextArea(e.target.value)} />
+        </div>
+        <div>
+          <Checkbox />
         </div>
       </div>
     </div>
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,8 +1,7 @@
 "use client"
 
-import * as ProgressPrimitive from "@radix-ui/react-progress"
-
 import * as React from "react"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
@@ -10,21 +9,20 @@ function Progress({
   className,
   value,
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root> & {
-  value?: number
-}) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "relative h-4 w-full overflow-hidden rounded-base border-2 border-border bg-secondary-background",
-        className,
+        "relative h-3 w-full overflow-hidden rounded-full",
+        "border bg-transparent",
+        className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="h-full w-full flex-1 border-r-2 border-border bg-main transition-all"
+        className="bg-pink-400 h-full w-full flex-1 transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
