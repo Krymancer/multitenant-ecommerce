44ded407e3dfa49215ae1521161c4fc5e0ae98cc
Júnior Nascimento <krymancer@protonmail.com>
2025-04-16 23:16:03 -0300

    feat: add footer and sidebar navigation components; update layout structure
    - Introduced a new Footer component for consistent page footer across the application.
    - Added NavbarSidebar component for improved mobile navigation experience.
    - Updated Layout component to include the new Footer and adjust layout structure for better responsiveness.
    - Enhanced Navbar with sidebar functionality and improved button styles for better user interaction.
    - Created new pages for About, Contact, Features, and Pricing with basic layout.

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -5,6 +5,9 @@ import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import NavbarSidebar from "./navbar-sidebar";
+import { MenuIcon } from "lucide-react";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -22,12 +25,19 @@ const navbarItems = [
 
 export default function Navbar() {
     const pathname = usePathname();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
                 <span className={cn("text-5xl font-semibold", poppins.className)}>funroad</span>
             </Link>
+
+            <NavbarSidebar
+                items={navbarItems}
+                open={isSidebarOpen}
+                onOpenChange={setIsSidebarOpen}
+            />
 
             <div className="flex items-center gap-4 hidden lg:flex">
                 {navbarItems.map((item) => (
@@ -40,7 +50,7 @@ export default function Navbar() {
             <div className="hidden lg:flex">
                 <Button
                     asChild
-                    variant="noShadow"
+                    variant="secondary"
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
                 >
                     <Link href="/sign-in">
@@ -49,12 +59,22 @@ export default function Navbar() {
                 </Button>
                 <Button
                     asChild
-                    variant="noShadow"
+                    variant="secondary"
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 hover:text-black transition-colors text-lg"
                 >
                     <Link href="/sign-up">
                         Start selling
                     </Link>
+                </Button>
+            </div>
+
+            <div className="lg:hidden flex items-center justify-center">
+                <Button
+                    variant="ghost"
+                    className="size-12 border-transparent bg-white"
+                    onClick={() => setIsSidebarOpen(true)}
+                >
+                    <MenuIcon />
                 </Button>
             </div>
         </nav>
@@ -64,11 +84,11 @@ export default function Navbar() {
 function NavbarItem({ href, children, isActive }: { href: string, children: React.ReactNode, isActive?: boolean }) {
     return (
         <Button
-            variant="noShadow"
+            variant="outline"
             asChild
             className={cn(
-                "bg-transparent border border-transparent hover:border hover:border-black rounded-full px-3.5 text-lg",
-                isActive && "bg-black text-white",
+                "bg-transparent hover:bg-transparent rounded-full hover:border-primary border-transparent",
+                isActive && "bg-black text-white hover:bg-black hover:text-white",
             )}
         >
             <Link href={href}>
