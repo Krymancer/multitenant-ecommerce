be71ac6701ee52d041377c9235f998523829fc7d
Junior Nascimento <krymancer@protonmail.com>
2025-05-06 23:53:06 -0300

    feat: implement product listing and detail views with infinite scrolling and loading states

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,26 +1,31 @@
+import type { SearchParams } from "nuqs/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 import { getQueryClient, trpc } from "@/trpc/server";
 
-import { ProductList, ProductListSkeleton } from "@/modules/products/ui/components/product-list";
-import { Suspense } from "react";
+import { loadProductFilters } from "@/modules/products/search-params";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
 
 export default async function SubcategoryPage(
-  { params }:
-    { params: Promise<{ subcategory: string }> }
+  { params,
+    searchParams
+  }: {
+    params: Promise<{ subcategory: string }>,
+    searchParams: Promise<SearchParams>
+  }
 ) {
   const { subcategory } = await params;
+  const filters = await loadProductFilters(searchParams);
 
   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({
     category: subcategory,
+    ...filters,
   }));
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<ProductListSkeleton />}>
-        <ProductList category={subcategory} />
-      </Suspense>
+      <ProductListView category={subcategory} />
     </HydrationBoundary>
   );
 }
